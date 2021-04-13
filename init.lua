80fa0f5ff4d9bb32328897fd32e1b521b3a1cb30
LucasMZReal <55422065+LucasMZReal@users.noreply.github.com>
2021-04-13 19:02:25 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -8,10 +8,10 @@ local os_clock = os.clock
 local table_find = table.find
 local table_insert = table.insert
 local array_remove = function(Table, idx)
-    local count = #Table
-    
-    Table[idx] = Table[count]
-    Table[count] = nil
+	local count = #Table
+
+	Table[idx] = Table[count]
+	Table[count] = nil
 end
 
 --\\ Yes, I micro-optimized it... Whatever.
@@ -22,9 +22,7 @@ Signal.__index = Signal;
 --[[
 local function runNoYield(func, ...)
     local args = ...;
-
     local toReturn;
-
     local thread = c_create(function()
         toReturn = table.pack(func(arguments))
     end)
@@ -40,60 +38,60 @@ end
 ]]
 
 function Signal.new()
-    return setmetatable({
-        Active = true;
-        _lastFired = os.clock();
-        _functions = {};
-    }, Signal)
+	return setmetatable({
+		Active = true;
+		_lastFired = os.clock();
+		_functions = {};
+		_threads = {};
+	}, Signal)
 end
 
 function Signal.GetLastFired(self)
-    return self._lastFired
+	return self._lastFired
 end
 
 function Signal.Connect(self, func)
-    local conn = setmetatable({
-        Connected = true;
-        _func = func;
-        _signal = self;
-    }, Signal)
+	local conn = setmetatable({
+		Connected = true;
+		_func = func;
+		_signal = self;
+	}, Signal)
 
-    table_insert(self._functions, conn)
-    return conn
+	table_insert(self._functions, conn)
+	return conn
 end
 
 function Signal.Disconnect(self)
-    if not self.Connected then return end
+	if not self.Connected then return end
 
-    local _signal = self._signal
-    local _functions = _signal._functions
-    
-    self.Connected = false;
-    self._signal = nil;
-    self._func = nil;
+	local _signal = self._signal
+	local _functions = _signal._functions
 
-    local connIndex = table_find(_functions, self)
-    if not connIndex then return end;
+	self.Connected = false;
+	self._signal = nil;
+	self._func = nil;
 
-    array_remove(_functions, connIndex)  
+	local connIndex = table_find(_functions, self)
+	if not connIndex then return end;
+
+	array_remove(_functions, connIndex)  
 end
 
 function Signal.Fire(self, ...)
-    local _functions = self._functions
-    local threads = {};
-    for i = 1, #_functions do
-        table_insert(threads, c_create(_functions[i]._func))
-    end
-    for i = 1, #threads do
-        c_resume(threads[i], ...)
-    end
-    table.clear(threads)
+	local _functions = self._functions
+	local threads = {};
+	for i = 1, #_functions do
+		table_insert(threads, c_create(_functions[i]._func))
+	end
+	for i = 1, #threads do
+		c_resume(threads[i], ...)
+		threads[i] = nil;
+	end
 end
 
 --[[
 function Signal.FireNoYield(self, ...)
     local _functions = self._functions
-
     for i = 1, #_functions do
         runNoYield(_functions[i], ...)
     end
@@ -102,28 +100,34 @@ end
 ]]
 
 function Signal.Wait(self)
-    local thread = c_running()
-    
-    local conn;
-    conn = self:Connect(function(...)
-        conn:Disconnect()
-        c_resume(thread, ...)
-    end)
-    
-    return c_yield()
+	local thread = c_running()
+
+	local conn;
+	conn = self:Connect(function(...)
+		conn:Disconnect()
+		c_resume(thread, ...)
+	end)
+
+	return c_yield()
 end
 
 function Signal.Destroy(self)
-    local _functions = self._functions
+	if not self.Active then return end
+	
+	local _functions = self._functions
 
-    self.Active = false
-    for i = 1, #_functions do
-        local conn = _functions[i]
-        conn.Connected = false;
-        conn._func = nil;
-        conn._signal = nil;
-    end
-    table.clear(_functions)
+	self.Active = false
+	for i = 1, #_functions do
+		local conn = _functions[i]
+		conn.Connected = false;
+		conn._func = nil;
+		conn._signal = nil;
+	end
+	table.clear(_functions)
+	self._functions = nil;
+	
+	table.clear(self._threads)
+	self._threads = nil;
 end
 
 return Signal
