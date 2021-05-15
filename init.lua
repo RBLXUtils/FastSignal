7fea03e6020d763da6acacfbed915c638cbcc2d7
LucasMZReal <55422065+LucasMZReal@users.noreply.github.com>
2021-05-15 19:36:28 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,65 +1,74 @@
-local HttpService = game:GetService('HttpService');
-
 local Signal = {};
 Signal.__index = Signal;
 Signal.ClassName = 'Signal';
+
+function Signal:IsA(...)
+	return ... == self.ClassName;
+end
+
+function Signal:IsActive()
+	return self._bindable ~= nil;
+end
+
+function Signal.new()
+	return setmetatable({
+		_bindable = Instance.new('BindableEvent')
+	}, Signal)
+end
+
+function Signal:Fire(...)
+	if not self._bindable then return end;
+
+	local fire_id = #self + 1;
+	self[fire_id] = table.pack(...);
+
+	self._bindable:Fire(fire_id);
+	self[fire_id] = nil;
+end
 
 function Signal.__call(self, _, ...)
 	return self:Connect(...);
 end
 
-function Signal.new()
-	local self = setmetatable({
-		_bindableEvent = Instance.new('BindableEvent');
-		Active = true;
-	}, Signal);
-	self._event = self._bindableEvent.Event;
-	return self;
-end
+function Signal:Connect(handle)
+	if not self._bindable then return end;
 
-function Signal:Connect(func)
-	if not self.Active then return end;
-
-	return self._event:Connect(function(fireId)
-		func(table.unpack(self[fireId]));
+	return self._bindable.Event:Connect(function(fire_id)
+		return handle(
+			table.unpack(
+				self[fire_id]
+			)
+		)
 	end)
 end
 
-function Signal:ConnectParallel(func)
-	if not self.Active then return end;
+function Signal:ConnectParallel(handle)
+	if not self._bindable then return end;
 
-	return self._event:ConnectParallel(function(fireId)
-		func(table.unpack(self[fireId]));
+	return self._bindable.Event:ConnectParallel(function(fire_id)
+		return handle(
+			table.unpack(
+				self[fire_id]
+			)
+		)
 	end)
-end
-
-function Signal:Fire(...)
-	if not self.Active then return end;
-
-	local fireId = os.clock();
-	if self[fireId] then return self:Fire(...) end;
-
-	self[fireId] = table.pack(...);
-	self._bindableEvent:Fire(fireId);
-	self[fireId] = nil;
 end
 
 function Signal:Wait()
-	if not self.Active then return end;
+	if not self._bindable then return end;
 
-	local fireId = self._event:Wait();
-	if not fireId then return end;
-
-	return table.unpack(self[fireId]);
+	return table.unpack(
+		self[
+			self._bindable:Wait()
+		]
+	)
 end
 
 function Signal:Destroy()
-	if not self.Active then return end;
+	if not self._bindable then return end;
 
-	self.Active = false;
-	self._event = nil;
-	self._bindableEvent:Destroy();
-	self._bindableEvent = nil;
+	self._bindable:Destroy();
+	self._bindable = nil;
 end
 
 return Signal;
