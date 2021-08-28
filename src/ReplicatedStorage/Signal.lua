cbf3fd32d150634276c64073ba0a49335d2faf3f
LucasMZ <55422065+LucasMZReal@users.noreply.github.com>
2021-08-28 01:54:22 -0300

    Update Signal.lua

--- src/ReplicatedStorage/Signal.lua
+++ src/ReplicatedStorage/Signal.lua
@@ -88,6 +88,34 @@
 			
 ]]
 
+--[[
+	License:
+
+	-----------------------------------------------------------------------------
+	MIT License
+
+	Copyright (c) 2021 LucasMZ
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+	-----------------------------------------------------------------------------
+]]
+
 local t_insert = table.insert
 local c_running = coroutine.running
 local c_yield = coroutine.yield
@@ -317,10 +345,6 @@ if not TOSTRING_ENABLED then
 end
 
 function Signal:__call(_, func)
-	if not self:IsActive() then
-		return
-	end
-
 	assert(
 		typeof(func) == 'function',
 		":Connect must be called with a function ".. self._name
