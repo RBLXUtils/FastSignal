90791b8b7ff6ff80c3e723b674468417795da3f6
LucasMZ <55422065+LucasMZReal@users.noreply.github.com>
2021-09-27 19:45:46 -0300

    Update Immediate.lua

--- src/ReplicatedStorage/Signal/Immediate.lua
+++ src/ReplicatedStorage/Signal/Immediate.lua
@@ -179,15 +179,17 @@ end
 function ScriptSignal:Fire(...)
 	local node = self._head
 	while node ~= nil do
-		if FreeThread == nil then
-			FreeThread = coroutine.create(RunHandlerInFreeThread)
-		end
+		if node._connection ~= nil then -- Is Connected
+			if FreeThread == nil then
+				FreeThread = coroutine.create(RunHandlerInFreeThread)
+			end
 
-		task.spawn(
-			FreeThread :: thread,
-			node._handle,
-			...
-		)
+			task.spawn(
+				FreeThread :: thread,
+				node._handle,
+				...
+			)
+		end
 
 		node = node._next
 	end
