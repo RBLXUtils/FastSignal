8ec14ba677a04fe35d6967615d4ca764a94c2d31
LucasMZ <55422065+LucasMZReal@users.noreply.github.com>
2021-11-01 20:58:02 -0300

    Fix typo

--- src/ReplicatedStorage/FastSignal/init.lua
+++ src/ReplicatedStorage/FastSignal/init.lua
@@ -311,7 +311,7 @@ function ScriptSignal:DisconnectAll()
 end
 
 --[=[
-	Destroys a ScriptSignal object, disconnecting all connection and making it unusable.
+	Destroys a ScriptSignal object, disconnecting all connections and making it unusable.
 
 	```lua
 	ScriptSignal:Destroy()
