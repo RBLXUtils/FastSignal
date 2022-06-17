654cc31b2ed54588fd015adca7be6760728281d3
LucasMZ <55422065+LucasMZReal@users.noreply.github.com>
2022-06-16 23:57:24 -0300

    Built and published by Moonwave

---
+++ assets/js/26065943.3116aaba.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[877],{82824:function(e){e.exports=JSON.parse('[{"type":"link","href":"/api/ScriptSignal","label":"\u200bScript\u200bSignal"},{"type":"link","href":"/api/ScriptConnection","label":"\u200bScript\u200bConnection"}]')}}]);
---
+++ assets/js/3c256cf5.ffa8ae5f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[914],{97868:function(s){s.exports=JSON.parse('{"sourceUrl":"https://github.com/RBLXUtils/FastSignal/blob/main","baseUrl":"/FastSignal/","classOrder":["ScriptSignal","ScriptConnection"],"apiCategories":[]}')}}]);
